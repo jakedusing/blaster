a77bf79fe2b4aecab499d3d63ea658839d67292b
Jake Dusing <dusingjake@gmail.com>
2023-11-28 21:09:02 -0600

    start elim announcements

--- Source/Blaster/PlayerController/BlasterPlayerController.h
+++ Source/Blaster/PlayerController/BlasterPlayerController.h
@@ -39,6 +39,9 @@ public:
 	float SingleTripTime = 0.f;
 
 	FHighPingDelegate HighPingDelegate;
+
+	void BroadcastElim(APlayerState* Attacker, APlayerState* Victim);
+
 protected:
 	virtual void BeginPlay() override;
 	void SetHUDTime();
@@ -76,6 +79,9 @@ protected:
 	void CheckPing(float DeltaTime);
 
 	void ShowReturnToMainMenu();
+
+	UFUNCTION(Client, Reliable)
+	void ClientElimAnnouncement(APlayerState* Attacker, APlayerState* Victim);
 
 private:
 	UPROPERTY()
